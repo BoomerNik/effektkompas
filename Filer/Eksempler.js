85f24df69a76e34ec806f5ddd68c5192ba443252
aartools <86226117+aartools@users.noreply.github.com>
2021-08-09 08:30:59 +0200

    Update Eksempler.js

--- Filer/Eksempler.js
+++ Filer/Eksempler.js
@@ -10,18 +10,6 @@ Eksempler.Blank =
 	"majTextSize": "3",
 	"minTextSize": "2"
 };
-
-Eksempler.Stor = 
-{
-	"title": "Stor Skabelon",
-	"icon": "Stor",
-	"text": "¤L1 r20 v\n\t\nTema 1 #y\n\tVirkemiddel a\n\tVirkemiddel b\n\tVirkemiddel c\n\tVirkemiddel d\n\t\n\t\tOplevelser a\n\t\tOplevelser b\n\n\t\t\tHandlinger a\n\t\t\tHandlinger b\n\nTema 2 #r\n\tVirkemiddel a\n\tVirkemiddel b\n\tVirkemiddel c\n\tVirkemiddel d\n\t\n\t\tOplevelser a\n\t\tOplevelser b\n\t\n\t\t\tHandlinger a\n\t\t\tHandlinger b\n\t\nTema 3 #g\n\tVirkemiddel a\n\tVirkemiddel b\n\tVirkemiddel c\n\tVirkemiddel d\n\t\n\t\tOplevelser a\n\t\tOplevelser b\n\t\n\t\t\tHandlinger a\n\t\t\tHandlinger b\n\t\nTema 4 #b3\n\tVirkemiddel a\n\tVirkemiddel b\n\tVirkemiddel c\n\tVirkemiddel d\n\t\n\t\tOplevelser a\n\t\tOplevelser b\n\t\n\t\t\tHandlinger a\n\t\t\tHandlinger b",
-	"rotation": "0",
-	"centerRadius": "15",
-	"titleTextSize": "3.7",
-	"majTextSize": "3",
-	"minTextSize": "2"
-};
 	
 Eksempler.Lille = 
 {
@@ -35,14 +23,27 @@ Eksempler.Lille =
 	"minTextSize": "2"
 };
 
+Eksempler.Stor = 
+{
+	"title": "Stor Skabelon",
+	"icon": "Stor",
+	"text": "¤L1 r20 v\n\t\nTema 1 #y\n\tVirkemiddel a\n\tVirkemiddel b\n\tVirkemiddel c\n\tVirkemiddel d\n\t\n\t\tOplevelse a\n\t\tOplevelse b\n\n\t\t\tHandling a\n\t\t\tHandling b\n\nTema 2 #r\n\tVirkemiddel a\n\tVirkemiddel b\n\tVirkemiddel c\n\tVirkemiddel d\n\t\n\t\tOplevelse a\n\t\tOplevelse b\n\t\n\t\t\tHandling a\n\t\t\tHandling b\n\t\nTema 3 #g\n\tVirkemiddel a\n\tVirkemiddel b\n\tVirkemiddel c\n\tVirkemiddel d\n\t\n\t\tOplevelse a\n\t\tOplevelse b\n\t\n\t\t\tHandling a\n\t\t\tHandling b\n\t\nTema 4 #b3\n\tVirkemiddel a\n\tVirkemiddel b\n\tVirkemiddel c\n\tVirkemiddel d\n\t\n\t\tOplevelse a\n\t\tOplevelse b\n\t\n\t\t\tHandling a\n\t\t\tHandling b",
+	"rotation": "0",
+	"centerRadius": "15",
+	"titleTextSize": "3.7",
+	"majTextSize": "3",
+	"minTextSize": "2"
+};
+
 Eksempler.Stor2 =
 {
 	"title": "Stor Skabelon 2",
 	"icon": "Urban",
-	"text": "¤L1 r20 v\n\t\nTema 1 #y\n\tVirkemiddel a\n\tVirkemiddel b\n\t\tOplevelser a\n\t\t\tHandlinger a\n\n\tVirkemiddel c\n\tVirkemiddel d\n\t\tOplevelser b\n\t\t\tHandlinger b\n\nTema 2 #r\n\tVirkemiddel a\n\tVirkemiddel b\n\t\tOplevelser a\n\t\t\tHandlinger a\n\n\tVirkemiddel c\n\tVirkemiddel d\n\t\tOplevelser b\n\t\t\tHandlinger b\n\t\nTema 3 #g\n\tVirkemiddel a\n\tVirkemiddel b\n\t\tOplevelser a\n\t\t\tHandlinger a\n\n\tVirkemiddel c\n\tVirkemiddel d\n\t\tOplevelser b\n\t\t\tHandlinger b\n\t\nTema 4 #b3\n\tVirkemiddel a\n\tVirkemiddel b\n\t\tOplevelser a\n\t\t\tHandlinger a\n\n\tVirkemiddel c\n\tVirkemiddel d\n\t\tOplevelser b\n\t\t\tHandlinger b",
+	"text": "¤L1 r20 v\n\t\nTema 1 #y\n\tVirkemiddel a\n\tVirkemiddel b\n\t\tOplevelse a\n\t\t\tHandling a\n\n\tVirkemiddel c\n\tVirkemiddel d\n\t\tOplevelse b\n\t\t\tHandling b\n\nTema 2 #r\n\tVirkemiddel a\n\tVirkemiddel b\n\t\tOplevelse a\n\t\t\tHandling a\n\n\tVirkemiddel c\n\tVirkemiddel d\n\t\tOplevelse b\n\t\t\tHandling b\n\t\nTema 3 #g\n\tVirkemiddel a\n\tVirkemiddel b\n\t\tOplevelse a\n\t\t\tHandling a\n\n\tVirkemiddel c\n\tVirkemiddel d\n\t\tOplevelse b\n\t\t\tHandling b\n\t\nTema 4 #b3\n\tVirkemiddel a\n\tVirkemiddel b\n\t\tOplevelse a\n\t\t\tHandling a\n\n\tVirkemiddel c\n\tVirkemiddel d\n\t\tOplevelse b\n\t\t\tHandling b",
 	"rotation": "0",
 	"centerRadius": "15",
 	"titleTextSize": "3.5",
 	"majTextSize": "3",
 	"minTextSize": "2"
 };
+
